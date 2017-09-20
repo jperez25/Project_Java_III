6c5b065f32a9ab72394e4459b735be706843c70e
abarrios1 <31938628+abarrios1@users.noreply.github.com>
2017-09-20 12:48:11 -0500

    Add files via upload

---
+++ JavaProjectMac/Java_3_project/src/login/First_Login.java
@@ -0,0 +1,33 @@
+package login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class First_Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // just load fxml file and display it in the stage:
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/screens/First_Login.fxml"));
+        //FXMLLoader loader2 = new FXMLLoader(getClass().getResource("RegisterScene.fxml"));
+        Parent root = loader.load();
+        //Parent root2 = loader2.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("AU parking");
+        primaryStage.show();
+    }
+
+    // main method to support non-JavaFX-aware environments:
+
+    public static void main(String[] args) {
+        // starts the FX toolkit, instantiates this class, 
+        // and calls start(...) on the FX Application thread:
+        launch(args); 
+    }
+}
