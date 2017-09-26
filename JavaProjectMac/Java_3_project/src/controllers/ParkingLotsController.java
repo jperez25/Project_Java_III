e0ebe174e4a1c0ca7b76d4495d70d2308aacb5e5
kschick02 <kschick02@aurora.edu>
2017-09-26 17:43:03 -0500

    Added logout link and dropdown box

--- JavaProjectMac/Java_3_project/src/controllers/ParkingLotsController.java
+++ JavaProjectMac/Java_3_project/src/controllers/ParkingLotsController.java
@@ -2,12 +2,17 @@ package controllers;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -22,8 +27,16 @@ public class ParkingLotsController {
 	private Label vagoSTLot;
 	@FXML
 	private Label vagoNTLot;
+	@FXML
+	private Hyperlink logoutBtn;
+	@FXML
+	private ComboBox<String> testDropdown;
 
 	public void initialize() throws Exception {
+		
+		String[] array = {"Vago North", "Vago South", "STEM", "Institute", "Southlawn", "Eckhart", "Dunham", "UBH"};
+		ObservableList<String> list = FXCollections.observableArrayList(array);
+		testDropdown.getItems().addAll(list);
 		
 		backBtn.setOnAction(e->{
 	        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/screens/First_Login.fxml"));
@@ -75,6 +88,26 @@ public class ParkingLotsController {
 		    alert.setHeaderText("Display info");
 		    alert.setContentText("We will show a lot of info about this parking lot\n just wait until our new vesion is released.");
 		    alert.showAndWait();
+		});
+		logoutBtn.setOnAction(e->{
+			Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+			alert.setTitle("Message");
+			alert.setHeaderText("Are you sure you want to logout?");
+			alert.showAndWait().ifPresent(response -> {
+				if (response == ButtonType.OK) {
+					FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/screens/First_Login.fxml"));
+			        Parent root2;
+					try {
+						root2 = loader2.load();
+				        Scene scene = new Scene(root2);
+						Stage stage = (Stage) backBtn.getScene().getWindow(); 
+						stage.setScene(scene);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+			 
 		});
 	}
 }
