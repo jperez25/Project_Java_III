ccad8040d61258ce7ac63c9eea3a1d4a86ab75bb
jperez25 <jova9991@gmail.com>
2017-09-28 18:05:29 -0500

    Some minor changes
    Just a little changes

--- Java_3_project/src/login/Controllers/ForgotController.java
+++ Java_3_project/src/login/Controllers/ForgotController.java
@@ -5,6 +5,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -107,6 +116,9 @@ public class ForgotController {
 							alert1.showAndWait();
 						}
 						 else {
+							 final String user = "auparkinglot@gmail.com";
+							 final String pass = "ParkingLot";
+							 
 							ResultSet eMail = stmt.executeQuery("select Email from user where Email='"+email+"';");
 							boolean isEmailEmpty = eMail.next();
 							if (!isEmailEmpty) {
@@ -117,13 +129,61 @@ public class ForgotController {
 								alert1.showAndWait();
 							}
 							else {
-								System.out.println("Code should be send");
-								System.out.println("Call method to send email");
+								Properties props = new Properties();
+								props.put("mail.smtp.auth", "true");
+								props.put("mail.smtp.starttls.enable", "true");
+								props.put("mail.smtp.host", "smtp.gmail.com");
+								props.put("mail.smtp.port", "587");
 								
-								ogPane.setVisible(false);
-								codePane.setLayoutY(ogPane.getLayoutY());
-								codePane.setLayoutX(ogPane.getLayoutX());
-								codePane.setVisible(true);
+								Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+									protected PasswordAuthentication getPasswordAuthentication() {
+										return new PasswordAuthentication(user, pass);
+									}
+								});
+								
+								try {
+									ResultSet emailConfirm = stmt.executeQuery("select email from user where username='"+username+"';");
+									emailConfirm.next();
+									//System.out.println(emailConfirm.getString(1));
+									
+									
+									//System.out.println(emailPass.getString(1));
+									Message message = new MimeMessage(session);
+									message.setFrom(new InternetAddress("auparkinglot@gmail.com")); //Hard coded who's sending the email
+									message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailConfirm.getString(1))); //Hard coded who the email is being sent to
+																												//Can use array or the database after being setup
+									message.setSubject("My first email"); //Subject of the email
+									ResultSet emailPass = stmt.executeQuery("select password from user where username='"+username+"';");
+									emailPass.next();
+									//ResultSet emailPass = stmt.executeQuery("select password from user where username='"+username+"';");
+									message.setContent("<h:body style=background-color:white;font-family:verdana; color:#002>" //Uses html for a basic setup for the email
+											+ "You're resetting your email!<br/>Your email is: "+ emailPass.getString(1) + "<br/>" //add email password from database in here
+											+ "</body>","text/html; charset=utf-8");
+									Transport.send(message); //Sends the message to the user
+									
+									Alert alert = new Alert(Alert.AlertType.INFORMATION); //Notify user that email was sent to their email address
+									alert.setTitle("Email sent");
+									alert.setHeaderText("Email was sent");
+									alert.setContentText("An email was sent to your account with your password.\n Try not to forget it this time.");
+									alert.showAndWait();
+									
+									//go to pick a lot
+									 FXMLLoader loader2 = new FXMLLoader(getClass().getResource("..\\Screens\\First_Login.fxml"));
+								     Parent root2;
+								     try {
+											root2 = loader2.load();
+									        Scene scene = new Scene(root2);
+											Stage stage = (Stage) backBtn.getScene().getWindow(); 
+											stage.setScene(scene);
+									} 
+								     catch (IOException e1) {
+											System.out.println(e);
+									}
+								} catch (MessagingException x) {
+									throw new RuntimeException(x);
+								}
+								
+								
 							}
 						}
 					 }
@@ -134,7 +194,10 @@ public class ForgotController {
 		});
 		//Check code and compare with the code send to user
 		verifyBtn.setOnAction(e->{
-			 String rescode = resTxtFl.getText();
+			ogPane.setVisible(false);
+			codePane.setLayoutY(ogPane.getLayoutY());
+			codePane.setLayoutX(ogPane.getLayoutX());
+			codePane.setVisible(true);
 			 
 		});
 	}
